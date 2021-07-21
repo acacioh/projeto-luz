acb085e2bbbf464f63f139e2deca2361c28d168e
AcacioH <acaciohenriqueafonso@gmail.com>
2021-07-21 15:40:07 -0300

    Estrutura inicial

---
+++ backend/src/main/java/br/ong/luz/projetoluz/ProjetoLuzApplication.java
@@ -0,0 +1,13 @@
+package br.ong.luz.projetoluz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLuzApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/ong/luz/projetoluz/ProjetoLuzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ong.luz.projetoluz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
